496c03ac2afd44bbb270e26f749f47e7c058c889
LetRicette <leticiaricette@id.uff.br>
2021-09-10 00:55:34 -0300

    Marcar como Favorito

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Settings settings = Settings();
   List<Meal> _avaliableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
   void _filterMeals(Settings settings) {
     setState(() {
@@ -36,6 +37,18 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -52,10 +65,11 @@ class _MyAppState extends State<MyApp> {
             )),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => TabsScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(_favoriteMeals),
         AppRoutes.CATEGORIES_MEALS: (ctx) =>
             CategoriesMealsScreen(_avaliableMeals),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) =>
+            MealDetailScreen(_toggleFavorite, _isFavorite),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen(settings, _filterMeals),
       },
     );
--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({Key? key}) : super(key: key);
+  List<Meal> favoriteMeals;
+  FavoriteScreen(this.favoriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text(
+          'Nenhuma refeição\nfoi marcada como favorita!',
+          style: TextStyle(
+            color: Colors.pink,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+      );
+    } else {
+      return ListView.builder(
+          itemCount: favoriteMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(favoriteMeals[index]);
+          });
+    }
   }
 }
